fe0f68c92a1f8c1ec5668e54b4ee1a27d3202dd4
AndresSP <50252749+Andres-Saavedra-Palomino@users.noreply.github.com>
2020-04-13 12:37:57 -0500

    Create main.js

---
+++ clase/main.js
@@ -0,0 +1,41 @@
+//iterador
+var i = 1
+//recursos / imagenes-rutas
+var banerImg = [
+	"https://via.placeholder.com/800x250",
+	"https://via.placeholder.com/700x250",
+	"https://via.placeholder.com/600x250",
+	"https://via.placeholder.com/500x250"]
+
+
+function activar(int) {
+	document.getElementById('imgbaner').src = banerImg[int]
+	i = int
+}
+
+function carrusel() {
+	var imagenes = document.getElementsByName('rbop')
+
+	setInterval(() => {
+		imagenes[i].checked = true
+		activar(i)
+		i++
+		i = i == 4 ? 0 : i
+	}, 1000);
+
+}
+
+function marcado() {
+	this.style = 'border:1px solid red'
+}
+function desmarcado() {
+	this.style = 'border:1px solid white'
+}
+
+function myHover() {
+	var art = document.getElementsByClassName('artpesca')
+	for (let a = 0; a < art.length; a++) {
+		art[a].addEventListener('mouseover', marcado)
+		art[a].addEventListener('mouseout', desmarcado)
+	}
+}
