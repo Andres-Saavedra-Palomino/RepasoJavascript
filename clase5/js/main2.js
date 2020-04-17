facf51d3014028c2d10bb4d4409764f2519ee9f4
AndresSP <andres.saavedrapal@gmail.com>
2020-04-17 08:06:32 -0500

    css ampliar y reducir

--- clase5/js/main2.js
+++ clase5/js/main2.js
@@ -1,7 +1,12 @@
 /* import { articulos_recursos, baners_recursos, paginasRedesSociales } from './recursos'*/
 var baners_recursos = [
-	"https://via.placeholder.com/900x250?text=BANER+1", "https://via.placeholder.com/900x250?text=BANER+2", "https://via.placeholder.com/900x250?text=BANER+3",]
-var paginasRedesSociales = ["https://www.facebook.com/", "https://www.twitter.com/", "https://www.instagram.com/"]
+	"https://via.placeholder.com/900x250?text=BANER+1",
+	"https://via.placeholder.com/900x250?text=BANER+2",
+	"https://via.placeholder.com/900x250?text=BANER+3",]
+var paginasRedesSociales = [
+	"https://www.facebook.com/",
+	"https://www.twitter.com/",
+	"https://www.instagram.com/"]
 var articulos_recursos = [
 	{ titulo: "articulo_1", imagen: "https://via.placeholder.com/400?text=Articulo+1" },
 	{ titulo: "articulo_2", imagen: "https://via.placeholder.com/400?text=Articulo+2" },
@@ -9,17 +14,17 @@ var articulos_recursos = [
 	{ titulo: "articulo_4", imagen: "https://via.placeholder.com/400?text=Articulo+4" },
 	{ titulo: "articulo_5", imagen: "https://via.placeholder.com/400?text=Articulo+5" },]
 
-//mis array enlaces de articulos
-var iconoRedesSociales = []
-function miseventos() {
-	iconoRedesSociales = document.getElementsByClassName('imgrs')
 
+//mis array enlaces de imagenes de redes sociales
+var iconoRedesSociales = document.getElementsByClassName('imgrs')
+function miseventos() {
 	for (const iterator of iconoRedesSociales) {
 		iterator.addEventListener('mouseover', ampliar)
 		iterator.addEventListener('mouseout', reducir)
 		iterator.addEventListener('click', social)
 	}
 }
+miseventos()
 //1
 function ampliar() {
 	this.classList.add("ampliar")
@@ -40,8 +45,8 @@ function social() {
 //4
 //iterador del Carousel
 var i = 1
+var miCarousel = document.getElementById('imgbaner')
 function carousel() {
-	var miCarousel = document.getElementById('imgbaner')
 
 	setInterval(() => {
 		miCarousel.src = baners_recursos[i]
@@ -51,22 +56,17 @@ function carousel() {
 }
 //5
 //array de los enlaces
-var enlaces = []
+var enlaces = document.getElementsByClassName('atipo')
+var artTitulo = document.getElementById('articuloTitulo')
+var artImagen = document.getElementById('articuloImagen')
 function seleccion() {
 	var it = Array.from(enlaces).indexOf(event.target)
-	var artTitulo = document.getElementById('articuloTitulo')
-	var artImagen = document.getElementById('articuloImagen')
 	artTitulo.innerText = articulos_recursos[it].titulo
 	artImagen.src = articulos_recursos[it].imagen
 }
 
-function alcargar() {
-	carousel()
-	miseventos()
-	enlaces = document.getElementsByClassName('atipo')
-
-	for (let index = 0; index < enlaces.length; index++) {
-		enlaces[index].addEventListener('click', seleccion)
-	}
+for (let index = 0; index < enlaces.length; index++) {
+	enlaces[index].addEventListener('click', seleccion)
 }
-alcargar()
+
+document.addEventListener('DOMContentLoaded', carousel)
