fd9aa1c7729d3c77d1c397f6fcfb4d39b931629e
AndresSP <andres.saavedrapal@gmail.com>
2020-05-01 22:33:47 -0500

    registro y login

--- login/signin.js
+++ login/signin.js
@@ -1,17 +1,24 @@
 let correo = document.getElementById('inputEmail')
 let pass = document.getElementById('inputPassword')
 
+
+
+let usuarioGuardado = sessionStorage.getItem('user')
+let contraseniaGuardada = sessionStorage.getItem('pass')
+
 let btn = document.getElementById('btn_registrar')
 btn.addEventListener('click', () => {
 	let user = correo.value
 	let ps = pass.value
 
+	if (usuarioGuardado && user == usuarioGuardado)
+		if (contraseniaGuardada && ps == contraseniaGuardada)
+			alert('Inicio de sesion exitoso')
+
+
 	sessionStorage.setItem('user', user)
 	sessionStorage.setItem('pass', ps)
-
-	if (user == 'dios' && ps == 123)
-		alert('Inicio de sesion exitoso')
-
 })
-console.log(sessionStorage.getItem('user'))
-console.log(sessionStorage.getItem('pass'))
+
+console.log({ usuarioGuardado })
+console.log({ contraseniaGuardada })
